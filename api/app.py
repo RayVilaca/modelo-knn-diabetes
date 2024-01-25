8b5f9678064f4756903cd20afe3ea4d83091431a
RayVilaca <cecilia_vilaca@outlook.com>
2024-01-25 20:50:48 -0300

    feat: add app

---
+++ api/app.py
@@ -0,0 +1,30 @@
+
+import pickle
+import warnings
+from flask import Flask, render_template, request, jsonify
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/prever', methods=['POST'])
+def prever():
+    
+    arq = open('data/modelo_knn_diabetes.pkl','rb')
+    modelo = pickle.load(arq)
+    arq.close()
+    
+    data = request.get_json()
+
+    # Processa os dados do formulário e faz previsões usando o modelo
+    # Retorna as previsões como JSON
+    resultado = modelo.predict([data])[0]
+    
+    return jsonify({'previsao': resultado})
+
+if __name__ == '__main__':
+    app.run(debug=True)
